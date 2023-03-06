5da7cfe86c1b5070302ba7af43ef37bf48dbcdde
Miguel-T24 <miguelt2296@gmail.com>
2023-03-05 19:59:23 -0500

    Add Components concept

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from "react"
 import logo from './logo.svg';
+import Componente from "./components/Component"
 import './App.css';
 
 function App() {
@@ -7,17 +9,23 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Course
         </p>
-        <a
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React
         </a>
       </header>
+
+      <section>
+        <Componente msg = "Hola I'm a Class Component"/>
+        <Componente msg = "Hola I'm a Functional"/>
+      </section>
+
     </div>
   );
 }
---
+++ src/components/Component.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+// Functional Components
+// class Componente extends Component{
+//     render(){
+//         return (
+//         // <h2>Hi I'm a component</h2>
+//         <h2>{this.props.msg}</h2>
+//         )
+//     }
+// }
+
+// Functional Components
+function Componente (props){
+    return <h2>{props.msg}</h2>
+}
+
+
+export default Componente
